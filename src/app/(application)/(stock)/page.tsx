58d1306c4ea131a36273c7c57e1156394ed84a27
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-05-28 15:32:39 -0300

    refactor: 💡 add SEO metadata: title, icon and description

--- src/app/(application)/(stock)/page.tsx
+++ src/app/(application)/(stock)/page.tsx
@@ -1,4 +1,9 @@
 import { Sidebar } from '@/components/sidebar';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Estoque'
+};
 
 export default function Home() {
   return (
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from 'next';
 import { LoginForm } from './components/loginForm';
+
+export const metadata: Metadata = {
+  title: 'Login'
+};
 
 export default function LoginPage() {
   return (
